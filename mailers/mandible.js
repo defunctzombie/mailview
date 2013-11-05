73cc06ad873eef6b534309224b6242a8ddd76ad5
Roman Shtylman <shtylman@gmail.com>
2013-11-05 13:09:55 -0500

    use mandible

--- mailers/mandible.js
+++ mailers/mandible.js
@@ -1,11 +1,6 @@
 var mandible = require('mandible');
 
 module.exports = function(dir, opts) {
-    var emailer = mandible(dir)
-
-    //mandible.html(name, locals, cb);
-    //mandible.txt(name, locals, cb);
-
-    return emailer;
+    return mandible(dir);
 };
 
